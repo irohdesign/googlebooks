84b64ed30ac80ac248fb57a758ff23713e6b0a0c
Jalia Evans <irohdesign@outlook.com>
2019-02-13 19:19:28 -0500

    added mongodb

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+        useMongoClient: true
+    }
+);
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
