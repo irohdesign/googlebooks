347be65084d468371c9ba73e7a5c6ca133c9758f
Jalia Evans <irohdesign@outlook.com>
2019-02-13 19:29:07 -0500

    setting up react and heroku

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
