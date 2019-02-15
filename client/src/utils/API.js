0929f7af10efbf97197e35899c03a202a39d1798
Jalia Evans <irohdesign@outlook.com>
2019-02-14 22:03:12 -0500

    commented out api page for now, focusing on the react pages

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default {
-    getBooks: function() {
-        return axios.get("/api/books", {params: {q: query}});
-    }
+    // getBooks: function() {
+    //     return axios.get("/api/books", {params: {q: query}});
+    // },
     // saveBooks: function() {
     //     return axios.post("/api/books/", bookData);
     // }
@@ -11,3 +11,5 @@ export default {
 
 // API Key
 // AIzaSyBG_sGuNRGKTP8NY6oCkXRohp7EyyCjXBo
+
+// issues with API key actually functioning, had to comment this page out otherwise it tanks the application
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,5 +3,9 @@ const router = require("express").Router();
 
 router.get("/books", (req, res) => {
     axios
-        .get("")
+        .get("", {params : req.query})
+        .then(({data: {results}}) => res.json(results))
+        .catch(err => res.status(422).json(err));
 })
+
+module.exports=router;
